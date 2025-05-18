4d0ab6412ab4ae95613f0013adaaf874297aa2d5
AldoSantos <lindualdo@hotmail.com>
2025-05-18 15:07:27 +0100

    feat: Add App.jsx component

---
+++ btc-turbo-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>BTC Turbo Dashboard</h1>
+      <p>Welcome to our Bitcoin Analytics Platform</p>
+    </div>
+  )
+}
+
+export default App
