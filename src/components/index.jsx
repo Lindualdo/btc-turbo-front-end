bdeb64d34473e01804f19c5e706ef4acdea9c85c
AldoSantos <lindualdo@hotmail.com>
2025-05-18 20:42:07 +0100

    Adiciona barrel exports para facilitar importação de componentes

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+// Arquivo de barrel export para componentes
+// Isso facilita a importação de múltiplos componentes de uma só vez
+
+// Exportando componentes individuais
+export { default as GaugeChart } from './GaugeChart/GaugeChart';
+export { default as BTCTendenciaPanel } from './BTCTendenciaPanel/BTCTendenciaPanel';
+
+// Adicione aqui outros componentes à medida que forem criados
